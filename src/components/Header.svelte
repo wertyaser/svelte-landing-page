5065860d02857e8ff26576e0bfc6e3f7b206f268
leonielNacman <nacmanleonielgarin@gmail.com>
2024-09-15 23:24:22 +0800

    first commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<header class="flex flex-col relative z-20">
+    <div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
+        <a href="/">
+            <h1 class="font-semibold">Swoley <span class="text-indigo-400">Moley</span></h1>
+        </a>
+
+    </div>
+
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import "../app.css";
+    import Footer from "../components/Footer.svelte";
+    import Header from "../components/Header.svelte";
+    import SectionWrapper from "../components/SectionWrapper.svelte";
+</script>
+
+<slot />
+
+<SectionWrapper>
+  <Header />
+</SectionWrapper>
+
+
+
+
