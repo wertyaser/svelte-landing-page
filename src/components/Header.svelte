81d03786da9b9f7170795bf1b0e874e5a8f842de
leonielNacman <nacmanleonielgarin@gmail.com>
2024-09-15 23:31:13 +0800

    modified:   src/components/Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,7 +7,16 @@
         <a href="/">
             <h1 class="font-semibold">Swoley <span class="text-indigo-400">Moley</span></h1>
         </a>
-
+        <button class="md:hidden grid place-items-center">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <nav class="hidden md:flex items-center gap-4 lg:gap-6">
+            <a class="duration-200 hover:text-indigo-400 cursor-pointer" href="">Product</a>
+            <a class="duration-200 hover:text-indigo-400 cursor-pointer" href="">Reviews</a>
+            <a class="duration-200 hover:text-indigo-400 cursor-pointer" href="">FAQ</a>
+            <button class="specialBtn">
+                <p>Start free</p>
+            </button>
+        </nav>
     </div>
-
 </header>
